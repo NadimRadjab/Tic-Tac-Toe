7bb86f7d939ea6d1510d5dc9924107c733e77dc4
Nadim Radjab <nadimradjab94@gmail.com>
2021-06-02 00:11:51 +0200

    Edit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const gameBoard = (() => {
     const h2 = document.querySelector('h2');
     const rButton = document.querySelector('#reset');
 
-    ///My table winCondtions and counter
+    ///Table winCondtions and counter
     let table = ['', '', '', '', '', '', '', '', ''];
     const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
     let counter = 0;
@@ -31,7 +31,7 @@ const gameBoard = (() => {
         return counter++;
     };
 
-    ///Makes the a Node list in to an Array so I can use all array methods
+    ///Makes the Node list in to an Array so I can use all array methods
     const theTable = (arr) => {
         return newarr = [...arr];
     };
@@ -56,7 +56,7 @@ const gameBoard = (() => {
 
 
     ///Test fucntion tests who wins the game.It loops over the winConditions.
-    //  Then looks at my table and looks if  X or the O is equal to the index of the winning condtion.
+    //  Then looks at the table and looks if  X or the O is equal to the index of the winning condtion.
     function winTest(board, side) {
         for (let i = 0; i < winConditions.length; i++) {
             let w = winConditions[i];
@@ -89,14 +89,14 @@ const gameBoard = (() => {
 ///How the game is played
 const displayController = (() => {
 
-    ///Selecting the elements.I have repeating variables from the upper module not a good practice.
+    ///Selecting the elements.I have repeating variables from the upper module.
     const divs = document.querySelectorAll('.play');
     const h2 = document.querySelector('h2');
     gameBoard.theTable(divs);
     let counter = 0;
 
 
-    ///Function that plays the game takes the an array
+    ///Function that plays the game takes the an array.
     function play(arr) {
 
         ///looping the arr so I display the divs
@@ -126,7 +126,7 @@ const displayController = (() => {
                     if (gameBoard.winTest(board, 'x') === true) {
                         counter = 0;
                         gameBoard.boardStyle();
-                        h2.textContent = "Player X Won";
+                        h2.textContent = "Player X Wins";
 
                     }
                 } else {
@@ -138,7 +138,7 @@ const displayController = (() => {
                     gameBoard.tableMarker('o', index);
                     h2.textContent = 'Player X Turn:';
                     if (gameBoard.winTest(board, 'o') === true) {
-                        h2.textContent = "Player O Won";
+                        h2.textContent = "Player O Wins";
                         counter = 0;
                         gameBoard.boardStyle();
                     }
@@ -148,7 +148,7 @@ const displayController = (() => {
                 ///When the counter goes to 9 no winner is declared.Stops the Game
                 if (counter === 9) {
                     counter = 0;
-                    h2.textContent = "It's a tie. Please restart the game";
+                    h2.textContent = "It's a draw. Please restart the game.";
                     gameBoard.boardStyle();
                 }
             });
